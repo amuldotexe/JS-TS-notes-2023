d6c4bf1f496b64f465d2bad19cfaabd6c0635c9b
amuldotexe <amul.exe@gmail.com>
2023-06-19 16:25:01 +0000

    folder structure change

---
+++ ChatGPT_Notes/ReadMe20230611.js
@@ -0,0 +1,15 @@
+/*
+This JS file will have the following details based on week 01
+
+- List of keywords that we got exposed to
+- List of how-to's that we got exposed to
+- List of key learnings about the journey of learning JS
+
+*/
+
+// List of keywords that we got exposed to
+
+/*
+- 
+
+*/
